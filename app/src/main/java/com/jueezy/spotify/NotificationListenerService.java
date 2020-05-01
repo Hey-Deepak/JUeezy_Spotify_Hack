72382cf0c0911c2321e4f37e1b1dff4f20095dc3
Lavish Swarnkar <lavishswarnkar@gmail.com>
2020-05-01 16:49:09 +0530

    code for rooted device added

--- app/src/main/java/com/jueezy/spotify/NotificationListenerService.java
+++ app/src/main/java/com/jueezy/spotify/NotificationListenerService.java
@@ -7,12 +7,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.service.notification.StatusBarNotification;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
 
 public class NotificationListenerService extends android.service.notification.NotificationListenerService {
     SharedPreferences sharedPreferences;
@@ -23,79 +27,86 @@ public class NotificationListenerService extends android.service.notification.No
 
     @Override
     public void onNotificationPosted(StatusBarNotification sbn) {
+        String pack = sbn.getPackageName();
+        if(!pack.equals("com.spotify.music"))
+            return;
+
         sharedPreferences = getSharedPreferences(SHARED_PREF, Context.MODE_PRIVATE);
         isMute = sharedPreferences.getBoolean("sw1", true);
-        isKill = sharedPreferences.getBoolean("sw2", false);
+        boolean isRootAccessGranted = RootUtil.isDeviceRooted();
+        isKill = sharedPreferences.getBoolean("sw2", false) && isRootAccessGranted;
         adsCounter = sharedPreferences.getInt("adsCounter", 0);
         songCounter = sharedPreferences.getInt("songCounter", 0);
+        boolean isAd = sbn.getNotification().actions.length == 3;
 
-        String pack = sbn.getPackageName();
-        String previousSong = sharedPreferences.getString("previousSong", "");;
-        String newSong = sharedPreferences.getString("previousSong", "");;
+        String previousSong = sharedPreferences.getString("previousSong", "")
+                , newSong;
+
         try {
-                previousSong = sharedPreferences.getString("previousSong", "");
-                Log.d("DDDD", "previous "+ previousSong);
-        } catch (NullPointerException ignored){
-
+            newSong = sbn.getNotification().extras.getCharSequence("android.text").toString();
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            return;
         }
-        try {
-                newSong = sbn.getNotification().extras.getCharSequence("android.text").toString();
-                Log.d("DDDD", "newSong1 "+ newSong);
-            } catch (NullPointerException ignored){
 
-        }
         pingMe(getApplicationContext());
 
-        if (RootUtil.isDeviceRooted() && isKill) {
-
-            if (pack.equals("com.spotify.music")) {
-                {
-                    Notification.Action[] actions = sbn.getNotification().actions;
-                    if (actions.length == 3) {
-                        ActivityManager am = (ActivityManager) getSystemService(Activity.ACTIVITY_SERVICE);
-                        am.killBackgroundProcesses("com.spotify.music");
-
-                        new Handler().post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Intent playSpotify = new Intent("com.spotify.mobile.android.ui.widget.NEXT");
-                                playSpotify.setPackage("com.spotify.music");
-                                sendBroadcast(playSpotify);
-                            }
-                        });
-                    }
+        if (isKill) {
+            Toast.makeText(this, "Kill Option", Toast.LENGTH_SHORT).show();
+            if (isAd) {
+                //Kill the app
+                Process suProcess = null;
+                try {
+                    suProcess = Runtime.getRuntime().exec("su");
+                    DataOutputStream os = new DataOutputStream(suProcess.getOutputStream());
+                    os.writeBytes("adb shell" + "\n");
+                    os.flush();
+                    os.writeBytes("am force-stop com.spotify.music" + "\n");
+                    os.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    Toast.makeText(this, "E : " + e, Toast.LENGTH_SHORT).show();
                 }
+
+                //Not working
+                /*ActivityManager am = (ActivityManager) getSystemService(Activity.ACTIVITY_SERVICE);
+                am.killBackgroundProcesses("com.spotify.music");*/
+
+                //Restart it
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent playSpotify = new Intent("com.spotify.mobile.android.ui.widget.NEXT");
+                        playSpotify.setPackage("com.spotify.music");
+                        sendBroadcast(playSpotify);
+                    }
+                }, 1000);
             }
         }
 
         if (isMute) {
-           /* Log.d("DC", "Mute On ^_^");
-            Toast.makeText(this, "Mute Option", Toast.LENGTH_SHORT).show();*/
-            if (pack.equals("com.spotify.music")) {
-                Toast.makeText(this, "Mute Option", Toast.LENGTH_SHORT).show();
-                if (newSong.equals(previousSong)) {
-                    Log.d("DDDD", "Equal");
-                    SharedPreferences.Editor editor = sharedPreferences.edit();     // play & Pause
-                    editor.putString("previousSong", previousSong);
-                    editor.apply();
-                } else {
-                    Log.d("DDDD", "UnEqual");
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putString("previousSong", newSong);                      // Next & Previous
-                    editor.putInt("songCounter", songCounter + 1);
-                    editor.apply();
-                }
-
-                Notification.Action[] actions = sbn.getNotification().actions;
-                if (actions.length == 3) {
-                    Toast.makeText(NotificationListenerService.this, "Ad Appeared", Toast.LENGTH_SHORT).show();
-                    SharedPreferences.Editor editor = sharedPreferences.edit();
-                    editor.putInt("adsCounter", adsCounter + 1);
-                    editor.apply();
-                    Muter.mute(getApplicationContext());
-                } else
-                    Muter.unMute(getApplicationContext());
+            Toast.makeText(this, "Mute Option", Toast.LENGTH_SHORT).show();
+            if (newSong.equals(previousSong)) {
+                Log.d("DDDD", "Equal");
+                SharedPreferences.Editor editor = sharedPreferences.edit();     // play & Pause
+                editor.putString("previousSong", previousSong);
+                editor.apply();
+            } else {
+                Log.d("DDDD", "UnEqual");
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString("previousSong", newSong);                      // Next & Previous
+                editor.putInt("songCounter", songCounter + 1);
+                editor.apply();
             }
+
+            if (isAd) {
+                Toast.makeText(NotificationListenerService.this, "Ad Appeared", Toast.LENGTH_SHORT).show();
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("adsCounter", adsCounter + 1);
+                editor.apply();
+                Muter.mute(getApplicationContext());
+            } else
+                Muter.unMute(getApplicationContext());
         }
     }
 
